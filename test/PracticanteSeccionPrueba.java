b692562c2441e79409d91010c3d7b914508411eb
Ameth-COOKING <bonillaameth@gmail.com>
2026-06-29 09:35:18 -0600

    agregue cambios

--- test/PracticanteSeccionPrueba.java
+++ test/PracticanteSeccionPrueba.java
@@ -1,8 +1,7 @@
+import logica.dao.excepciones.RegistroDuplicadoExcepcion;
 import logica.dao.excepciones.UsuariosExcepcion;
 import logica.dao.objetos.PracticanteSeccionDao;
-import logica.dao.objetos.SeccionDao;
 import logica.dominio.PracticanteSeccion;
-import logica.dominio.Seccion;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,28 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class PracticanteSeccionPrueba {
 
     @Test
-    public void pruebaAgregarPracticanteSeccionExitoso() throws UsuariosExcepcion {
-        SeccionDao seccionDao = new SeccionDao();
-        seccionDao.agregarSeccion(new Seccion("E100", "2026-2"));
-        PracticanteSeccion ps = new PracticanteSeccion("S24013282", "E100");
+    public void pruebaAgregarPracticanteSeccionExitoso() throws UsuariosExcepcion, RegistroDuplicadoExcepcion {
+        PracticanteSeccion ps = new PracticanteSeccion("S24013282", "NRC-11111", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
         int filasAfectadas = practicanteSeccionDao.agregarPracticanteSeccion(ps);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
-    public void pruebaAgregarPracticanteSeccionAlternoOtraPracticante() throws UsuariosExcepcion {
-        PracticanteSeccion ps = new PracticanteSeccion("S22019874", "E100");
+    public void pruebaAgregarPracticanteSeccionAlternoOtraPracticante() throws UsuariosExcepcion, RegistroDuplicadoExcepcion {
+        PracticanteSeccion ps = new PracticanteSeccion("S22019874", "NRC-11111", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
         int filasAfectadas = practicanteSeccionDao.agregarPracticanteSeccion(ps);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
-    public void pruebaAgregarPracticanteSeccionAlternoOtraSeccion() throws UsuariosExcepcion {
-        SeccionDao seccionDao = new SeccionDao();
-        seccionDao.agregarSeccion(new Seccion("F100", "2026-2"));
-        PracticanteSeccion ps = new PracticanteSeccion("S23010045", "F100");
+    public void pruebaAgregarPracticanteSeccionAlternoOtraSeccion() throws UsuariosExcepcion, RegistroDuplicadoExcepcion {
+        PracticanteSeccion ps = new PracticanteSeccion("S23010045", "NRC-22222", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
         int filasAfectadas = practicanteSeccionDao.agregarPracticanteSeccion(ps);
         assertEquals(1, filasAfectadas);
@@ -39,60 +34,67 @@ public class PracticanteSeccionPrueba {
 
     @Test
     public void pruebaAgregarPracticanteSeccionDuplicadaLanzaExcepcion() {
-        PracticanteSeccion ps = new PracticanteSeccion("S24013282", "E100");
+        PracticanteSeccion ps = new PracticanteSeccion("S24013282", "NRC-11111", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
-        assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.agregarPracticanteSeccion(ps));
+        assertThrows(RegistroDuplicadoExcepcion.class, () -> practicanteSeccionDao.agregarPracticanteSeccion(ps));
     }
 
     @Test
     public void pruebaAgregarPracticanteSeccionExcepcionMatriculaNula() {
-        PracticanteSeccion ps = new PracticanteSeccion(null, "E100");
+        PracticanteSeccion ps = new PracticanteSeccion(null, "NRC-11111", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
         assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.agregarPracticanteSeccion(ps));
     }
 
     @Test
     public void pruebaAgregarPracticanteSeccionExcepcionSeccionNula() {
-        PracticanteSeccion ps = new PracticanteSeccion("S24013282", null);
+        PracticanteSeccion ps = new PracticanteSeccion("S24013282", null, 1);
+        PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
+        assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.agregarPracticanteSeccion(ps));
+    }
+
+    @Test
+    public void pruebaAgregarPracticanteSeccionExcepcionMatriculaNoExistente() {
+        PracticanteSeccion ps = new PracticanteSeccion("S99999999", "NRC-11111", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
         assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.agregarPracticanteSeccion(ps));
     }
 
     @Test
     public void pruebaModificarPracticanteSeccionExitoso() throws UsuariosExcepcion {
-        PracticanteSeccion ps = new PracticanteSeccion("S24013282", "E100");
+        PracticanteSeccion ps = new PracticanteSeccion("S24013282", "NRC-22222", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
-        int filasAfectadas = practicanteSeccionDao.modificarPracticanteSeccion("S24013282", "E100", ps);
+        int filasAfectadas = practicanteSeccionDao.modificarPracticanteSeccion("S24013282", "NRC-11111", 1, ps);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
     public void pruebaModificarPracticanteSeccionAlternoOtraPracticante() throws UsuariosExcepcion {
-        PracticanteSeccion ps = new PracticanteSeccion("S22019874", "E100");
+        PracticanteSeccion ps = new PracticanteSeccion("S22019874", "NRC-22222", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
-        int filasAfectadas = practicanteSeccionDao.modificarPracticanteSeccion("S22019874", "E100", ps);
+        int filasAfectadas = practicanteSeccionDao.modificarPracticanteSeccion("S22019874", "NRC-11111", 1, ps);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
     public void pruebaModificarPracticanteSeccionAlternoNoExistente() throws UsuariosExcepcion {
-        PracticanteSeccion ps = new PracticanteSeccion("S99999999", "Z999");
+        PracticanteSeccion ps = new PracticanteSeccion("S99999999", "NRC-99999", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
-        int filasAfectadas = practicanteSeccionDao.modificarPracticanteSeccion("S99999999", "Z999", ps);
+        int filasAfectadas = practicanteSeccionDao.modificarPracticanteSeccion("S99999999", "NRC-99999", 1, ps);
         assertEquals(0, filasAfectadas);
     }
 
     @Test
     public void pruebaModificarPracticanteSeccionExcepcionMatriculaNula() {
-        PracticanteSeccion ps = new PracticanteSeccion(null, "E100");
+        PracticanteSeccion ps = new PracticanteSeccion(null, "NRC-11111", 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
-        assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.modificarPracticanteSeccion(null, "E100", ps));
+        assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.modificarPracticanteSeccion(null, "NRC-11111", 1, ps));
     }
 
     @Test
     public void pruebaModificarPracticanteSeccionExcepcionSeccionNula() {
-        PracticanteSeccion ps = new PracticanteSeccion("S24013282", null);
+        PracticanteSeccion ps = new PracticanteSeccion("S24013282", null, 1);
         PracticanteSeccionDao practicanteSeccionDao = new PracticanteSeccionDao();
-        assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.modificarPracticanteSeccion("S24013282", null, ps));
+        assertThrows(UsuariosExcepcion.class, () -> practicanteSeccionDao.modificarPracticanteSeccion("S24013282", null, 1, ps));
     }
 }
--- test/SeccionPrueba.java
+++ test/SeccionPrueba.java
@@ -1,3 +1,4 @@
+import logica.dao.excepciones.RegistroDuplicadoExcepcion;
 import logica.dao.excepciones.UsuariosExcepcion;
 import logica.dao.objetos.SeccionDao;
 import logica.dominio.Seccion;
@@ -11,87 +12,94 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SeccionPrueba {
 
     @Test
-    public void pruebaAgregarSeccionExitoso() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("A010", "2026-2");
+    public void pruebaAgregarSeccionExitoso() throws UsuariosExcepcion, RegistroDuplicadoExcepcion {
+        Seccion seccion = new Seccion("NRC-11111", 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
         int filasAfectadas = seccionDao.agregarSeccion(seccion);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
-    public void pruebaAgregarSeccionAlternoPeriodoDiferente() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("B010", "2026-1");
+    public void pruebaAgregarSeccionAlternoOtroProfesor() throws UsuariosExcepcion, RegistroDuplicadoExcepcion {
+        Seccion seccion = new Seccion("NRC-22222", 1, "PROF-002");
         SeccionDao seccionDao = new SeccionDao();
         int filasAfectadas = seccionDao.agregarSeccion(seccion);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
-    public void pruebaAgregarSeccionAlternoLetraDiferente() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("C010", "2026-2");
+    public void pruebaAgregarSeccionAlternoMismoNRCPeriodoDiferente() throws UsuariosExcepcion, RegistroDuplicadoExcepcion {
+        Seccion seccion = new Seccion("NRC-33333", 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
         int filasAfectadas = seccionDao.agregarSeccion(seccion);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
-    public void pruebaAgregarSeccionDuplicadaLanzaExcepcion() {
-        Seccion seccion = new Seccion("A010", "2026-2");
+    public void pruebaAgregarSeccionDuplicadaEnMismoPeriodoLanzaExcepcion() {
+        Seccion seccion = new Seccion("NRC-11111", 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
-        assertThrows(UsuariosExcepcion.class, () -> seccionDao.agregarSeccion(seccion));
+        assertThrows(RegistroDuplicadoExcepcion.class, () -> seccionDao.agregarSeccion(seccion));
     }
 
     @Test
     public void pruebaAgregarSeccionExcepcionNoSeccionNulo() {
-        Seccion seccion = new Seccion(null, "2026-2");
+        Seccion seccion = new Seccion(null, 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
         assertThrows(UsuariosExcepcion.class, () -> seccionDao.agregarSeccion(seccion));
     }
 
     @Test
-    public void pruebaAgregarSeccionExcepcionPeriodoNulo() {
-        Seccion seccion = new Seccion("D010", null);
+    public void pruebaAgregarSeccionExcepcionProfesorNulo() {
+        Seccion seccion = new Seccion("NRC-44444", 1, null);
+        SeccionDao seccionDao = new SeccionDao();
+        assertThrows(UsuariosExcepcion.class, () -> seccionDao.agregarSeccion(seccion));
+    }
+
+    @Test
+    public void pruebaAgregarSeccionExcepcionProfesorNoExistente() {
+        Seccion seccion = new Seccion("NRC-55555", 1, "PROF-999");
         SeccionDao seccionDao = new SeccionDao();
         assertThrows(UsuariosExcepcion.class, () -> seccionDao.agregarSeccion(seccion));
     }
 
     @Test
     public void pruebaModificarSeccionExitoso() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("A010", "2027-1");
+        Seccion seccion = new Seccion("NRC-11111", 1, "PROF-002");
         SeccionDao seccionDao = new SeccionDao();
-        int filasAfectadas = seccionDao.modificarSeccion("A010", seccion);
+        int filasAfectadas = seccionDao.modificarSeccion("NRC-11111", seccion);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
-    public void pruebaModificarSeccionAlternoCambioPeriodo() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("B010", "2027-2");
+    public void pruebaModificarSeccionAlternoCambioProfesor() throws UsuariosExcepcion {
+        Seccion seccion = new Seccion("NRC-22222", 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
-        int filasAfectadas = seccionDao.modificarSeccion("B010", seccion);
+        int filasAfectadas = seccionDao.modificarSeccion("NRC-22222", seccion);
         assertEquals(1, filasAfectadas);
     }
 
     @Test
     public void pruebaModificarSeccionAlternoNoExistente() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("Z999", "2026-2");
+        Seccion seccion = new Seccion("NRC-99999", 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
-        int filasAfectadas = seccionDao.modificarSeccion("Z999", seccion);
+        int filasAfectadas = seccionDao.modificarSeccion("NRC-99999", seccion);
         assertEquals(0, filasAfectadas);
     }
 
     @Test
-    public void pruebaModificarSeccionAlternoNoSeccionNuloRetornaCero() throws UsuariosExcepcion {
-        Seccion seccion = new Seccion("A010", "2027-1");
+    public void pruebaModificarSeccionNoSeccionNuloRetornaCero() throws UsuariosExcepcion {
+        Seccion seccion = new Seccion("NRC-11111", 1, "PROF-001");
         SeccionDao seccionDao = new SeccionDao();
         int filasAfectadas = seccionDao.modificarSeccion(null, seccion);
         assertEquals(0, filasAfectadas);
     }
 
     @Test
-    public void pruebaModificarSeccionExcepcionPeriodoNulo() {
-        Seccion seccion = new Seccion("A010", null);
+    public void pruebaModificarSeccionExcepcionProfesorNulo() {
+        Seccion seccion = new Seccion("NRC-11111", 1, null);
         SeccionDao seccionDao = new SeccionDao();
-        assertThrows(UsuariosExcepcion.class, () -> seccionDao.modificarSeccion("A010", seccion));
+        assertThrows(UsuariosExcepcion.class, () -> seccionDao.modificarSeccion("NRC-11111", seccion));
     }
 
     @Test
@@ -118,11 +126,32 @@ public class SeccionPrueba {
     }
 
     @Test
-    public void pruebaObtenerSeccionesTienenPeriodo() throws UsuariosExcepcion {
+    public void pruebaObtenerSeccionesTienenProfesor() throws UsuariosExcepcion {
         SeccionDao seccionDao = new SeccionDao();
         List<Seccion> secciones = seccionDao.obtenerSecciones();
         for (Seccion seccion : secciones) {
-            assertNotNull(seccion.getPeriodo());
+            assertNotNull(seccion.getNumPersonalProfesor());
         }
+    }
+
+    @Test
+    public void pruebaObtenerSeccionesPorProfesorExitoso() throws UsuariosExcepcion {
+        SeccionDao seccionDao = new SeccionDao();
+        List<Seccion> secciones = seccionDao.obtenerSeccionesPorProfesor("PROF-001");
+        assertNotNull(secciones);
+    }
+
+    @Test
+    public void pruebaObtenerSeccionesPorProfesorAlternoOtroProfesor() throws UsuariosExcepcion {
+        SeccionDao seccionDao = new SeccionDao();
+        List<Seccion> secciones = seccionDao.obtenerSeccionesPorProfesor("PROF-002");
+        assertNotNull(secciones);
+    }
+
+    @Test
+    public void pruebaObtenerSeccionesPorProfesorNoExistente() throws UsuariosExcepcion {
+        SeccionDao seccionDao = new SeccionDao();
+        List<Seccion> secciones = seccionDao.obtenerSeccionesPorProfesor("PROF-999");
+        assertEquals(0, secciones.size());
     }
 }
